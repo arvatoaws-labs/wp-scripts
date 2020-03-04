823f4b10c70022052b3f028d7673fb5e899ac8b0
Philipp <philipp.hellmich@bertelsmann.de>
2020-03-04 15:59:58 +0100

    fix

--- install-wp-core.sh
+++ install-wp-core.sh
@@ -3,5 +3,35 @@ set -e
 
 cd /app
 
+if [ "$WORDPRESS_USERNAME" = "" ]
+then
+   echo "please define WORDPRESS_USERNAME"
+   exit 1
+fi
+
+if [ "$WORDPRESS_PASSWORD" = "" ]
+then
+   echo "please define WORDPRESS_PASSWORD"
+   exit 1
+fi
+
+if [ "$WORDPRESS_BLOGURL" = "" ]
+then
+   echo "please define WORDPRESS_BLOGURL"
+   exit 1
+fi
+
+if [ "$WORDPRESS_BLOGNAME" = "" ]
+then
+   echo "please define WORDPRESS_BLOGNAME"
+   exit 1
+fi
+
+if [ "$WORDPRESS_EMAIL" = "" ]
+then
+   echo "please define WORDPRESS_BLOGNAME"
+   exit 1
+fi
+
 echo "installing wp core if necessary..."
 wp core is-installed --allow-root || wp core install --url="$WORDPRESS_BLOGURL" --title="$WORDPRESS_BLOGNAME" --admin_user="$WORDPRESS_USERNAME" --admin_password="$WORDPRESS_PASSWORD" --admin_email="$WORDPRESS_EMAIL" --skip-email --allow-root
--- update-wp-admin-user.sh
+++ update-wp-admin-user.sh
@@ -3,5 +3,23 @@ set -e
 
 cd /app
 
+if [ "$WORDPRESS_USERNAME" = "" ]
+then
+   echo "please define WORDPRESS_USERNAME"
+   exit 1
+fi
+
+if [ "$WORDPRESS_PASSWORD" = "" ]
+then
+   echo "please define WORDPRESS_PASSWORD"
+   exit 1
+fi
+
+if [ "$WORDPRESS_EMAIL" = "" ]
+then
+   echo "please define WORDPRESS_BLOGNAME"
+   exit 1
+fi
+
 echo "updating admin user settings..."
 wp user update $WORDPRESS_USERNAME --user_pass="$WORDPRESS_PASSWORD" --user_email="$WORDPRESS_EMAIL" --skip-email --allow-root

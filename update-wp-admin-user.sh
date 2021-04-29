b0ee1c027b634194239bf1a2c09b5baea8ee04cd
Philipp Hellmich <philipp.hellmich@bertelsmann.de>
2021-04-29 16:09:12 +0200

    added multi user support

--- update-wp-admin-user.sh
+++ update-wp-admin-user.sh
@@ -26,3 +26,37 @@ echo "using wordpress-email $WORDPRESS_EMAIL"
 
 echo "create admin user if not exists otherwise update admin user..."
 wp user list --field=user_login | grep "$WORDPRESS_USERNAME" && wp user update $WORDPRESS_USERNAME --user_pass="$WORDPRESS_PASSWORD" --user_email="$WORDPRESS_EMAIL" --skip-email --allow-root || wp user create "$WORDPRESS_USERNAME" "$WORDPRESS_EMAIL" --role=administrator --user_pass="$WORDPRESS_PASSWORD"
+
+for number in {2..9}; do
+
+   wp_user="WORDPRESS_USERNAME_$number"
+   wp_pass="WORDPRESS_PASSWORD_$number"
+   wp_email="WORDPRESS_EMAIL_$number"
+
+   WORDPRESS_USERNAME="${!wp_user}"
+   WORDPRESS_PASSWORD="${!wp_pass}"
+   WORDPRESS_EMAIL="${!wp_email}"
+
+   if [ "$WORDPRESS_USERNAME" = "" ]
+   then
+      continue
+   fi
+
+   if [ "$WORDPRESS_PASSWORD" = "" ]
+   then
+      continue
+   fi
+
+   if [ "$WORDPRESS_EMAIL" = "" ]
+   then
+      continue
+   fi
+
+   echo "using wordpress-username $WORDPRESS_USERNAME"
+   echo "using wordpress-email $WORDPRESS_EMAIL"
+
+   echo "create admin user if not exists otherwise update admin user..."
+   wp user list --field=user_login | grep "$WORDPRESS_USERNAME" && wp user update $WORDPRESS_USERNAME --user_pass="$WORDPRESS_PASSWORD" --user_email="$WORDPRESS_EMAIL" --skip-email --allow-root || wp user create "$WORDPRESS_USERNAME" "$WORDPRESS_EMAIL" --role=administrator --user_pass="$WORDPRESS_PASSWORD"
+
+done
+exit 0

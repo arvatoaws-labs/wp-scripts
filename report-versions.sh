32c876189d36437e72ea60a6691e2615c95e5e34
salihoglu87 <30071247+salihoglu87@users.noreply.github.com>
2023-03-09 17:58:13 +0100

    add curl retry and fix typo

--- report-versions.sh
+++ report-versions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 #check if APP_STATUS_API and STATUS_API_KEY are not empty
-if [[ -n "$APP_STATUS_API" && -n "$STATUS_API_KEY" ]]; then
+if [[ -n "$STATUS_API_URL" && -n "$STATUS_API_KEY" ]]; then
   
-WP_PROJECT=$(echo "$HOSTNAME" | cut -d'-' -f1-2)
 # Detect the PHP, WordPress, and Node.js versions
 PHP_VERSION=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
 WP_VERSION=$(wp core version --skip-plugins --skip-themes --skip-packages | cut -d '.' -f 1,2)
@@ -12,7 +11,6 @@ WP_VERSION=$(wp core version --skip-plugins --skip-themes --skip-packages | cut 
 JSON_PAYLOAD=$(cat <<EOF
 {
     "category": "wordpress",
-    "project": "$WP_PROJECT",
     "domain": "$WP_FORCE_HOST",
     "products": [
         {
@@ -30,10 +28,16 @@ EOF
 # echo $JSON_PAYLOAD
 #   Send the HTTP POST request with curl
 curl -X POST \
-     -H "Content-Type: application/json" \
-     -H "x-api-key: $STATUS_API_KEY" \
-     -d "$JSON_PAYLOAD" \
-        "$STATUS_API_ENDPOINT"
+    --max-time 3 \
+    --retry 3 \
+    --retry-all-errors \
+    --retry-delay 3 \
+    --retry-max-time 18 \
+    -H "Content-Type: application/json" \
+    -H "x-api-key: $STATUS_API_KEY" \
+    -d "$JSON_PAYLOAD" \
+       "$STATUS_API_URL"
 else
   wp core version --extra
+  php -v
 fi
